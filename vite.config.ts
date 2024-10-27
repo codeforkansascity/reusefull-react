f852f3d42585355d3f4f571d2d039e944930185e
RobKraft <rob@robkraft.org>
2024-10-27 18:50:42 -0500

    Update vite.config.ts revert changes

--- vite.config.ts
+++ vite.config.ts
@@ -49,10 +49,10 @@ export default defineConfig({
             }
         },
         port: 5173,
-        https: false
-            // {
-            //# key: fs.readFileSync(keyFilePath),
-            //cert: fs.readFileSync(certFilePath),
-        //}
+        https: 
+            {
+            # key: fs.readFileSync(keyFilePath),
+            cert: fs.readFileSync(certFilePath),
+        }
     }
 })
