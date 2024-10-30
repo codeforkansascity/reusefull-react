34e9f11655595a934b08fafb03ef231761afaab3
dross20 <drew.e.ross@gmail.com>
2024-10-30 17:06:25 -0500

    Update checkboxes to be consistent, add header and new buttons

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,14 @@ import CharityForm from './DonationCharityForm/DonationCharityForm.tsx'
 function App() {
    return (
          <>
+         <div className='appContainer'>
+            <h2 className='contrastText'>Tell us more about your items and preferences</h2>
+            <button className='resetButton'>Reset all selections</button>
+         </div>
          <DonationForm/>
          <ItemForm/>
         <CharityForm/>
+         <button className='submitButton'>Continue to results</button>
         </>
    );
 }
