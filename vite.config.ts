b64e84ac50c69ac3e4fb8a59f260e4a64200cc51
tim vanlerberg <tim@brightsidedeveloper.com>
2025-09-18 20:00:38 -0500

    Localhost working

--- vite.config.ts
+++ vite.config.ts
@@ -1,68 +1,16 @@
-import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import fs from 'fs';
-import path from 'path';
-import child_process from 'child_process';
-import { env } from 'process';
-import type { ServerOptions } from 'vite';
-
-// Skip certificate generation in CI environment
-const isCI = !!process.env.CI;
-
-let httpsSettings: false | { key: Buffer; cert: Buffer } = false;
-
-if (!isCI) {
-    const baseFolder =
-        env.APPDATA !== undefined && env.APPDATA !== ''
-            ? `${env.APPDATA}/ASP.NET/https`
-            : `${env.HOME}/.aspnet/https`;
-
-    const certificateName = "reusefull-react.client";
-    const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
-    const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
-
-    if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
-        if (0 !== child_process.spawnSync('dotnet', [
-            'dev-certs',
-            'https',
-            '--export-path',
-            certFilePath,
-            '--format',
-            'Pem',
-            '--no-password',
-        ], { stdio: 'inherit', }).status) {
-            throw new Error("Could not create certificate.");
-        }
-    }
-
-    httpsSettings = {
-        key: fs.readFileSync(keyFilePath),
-        cert: fs.readFileSync(certFilePath),
-    };
-}
-
-const target = env.ASPNETCORE_HTTPS_PORT 
-    ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` 
-    : env.ASPNETCORE_URLS 
-        ? env.ASPNETCORE_URLS.split(';')[0] 
-        : 'https://localhost:7246';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
-    server: {
-        proxy: {
-            '^/weatherforecast': {
-                target,
-                secure: false
-            }
-        },
-        port: 5173,
-        https: httpsSettings as ServerOptions['https']
-    }
-});
+  },
+  server: {
+    port: 5173,
+    open: true,
+  },
+})
